b0a4a70b007914e27b7ce8f59d13b0c5450bc0fc
Ronny Ewanek <ronny.ewanek@civiq.com>
2017-01-03 16:43:56 -0600

    add register attempt

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../shared/models/user.model'; 
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+   
 
-  constructor() { }
+  // model = new User('Ronny', 'ronny@gmail.com', 'password');
+
+  // user = new User(); 
+  
+  submitted = false; 
+
+ 
+
+
+
+  constructor(user: User) {
+
+    onSubmit(user.username, user.email, user.password) {
+      this.submitted = true; 
+
+      new user(this.username, this.email, this.password);
+    }
+
+  }
 
   ngOnInit() {
   }
+
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/services/user.service';
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+   
 
   constructor(private userService: UserService) { }
   profile = {};
@@ -22,6 +26,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+// get diagnostic() { return JSON.stringify(this.model)}
 }
 // /Users/ronnyewanek/Desktop/conduit-app/src/app/shared/services/user.service.
 
